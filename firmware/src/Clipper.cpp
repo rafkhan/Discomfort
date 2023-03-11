98522238ac98ba0329c523fb16960a72f55da922
Rafael Khan <raf.khan.f@gmail.com>
2023-03-11 08:15:27 -0500

    hard clip

--- firmware/src/Clipper.cpp
+++ firmware/src/Clipper.cpp
@@ -1,6 +1,25 @@
 #include <cmath>
 #include "Clipper.h"
+#include "util.h"
 
 float Clipper::clip(float input, float gain, ClipperType type) {
-  return tanh(input * gain);
+  float y = input * gain;
+  if (type == CLIPPER_SOFT) {
+    return tanh(y);
+  }
+
+  if (type == CLIPPER_ALT) {
+    return sin(y);
+  }
+
+  // Hard clip
+  if (y > 1.0f) {
+    return 1.0f;
+  }
+
+  if (y < -1.0f) {
+    return -1.0f;
+  }
+
+  return y;
 }
--- firmware/src/util.h
+++ firmware/src/util.h
@@ -2,5 +2,6 @@
 #define DISCOMFORT_UTIL_H
 
 float map(float x, float in_min, float in_max, float out_min, float out_max);
+template <typename T> int sgn(T val);
 
 #endif
