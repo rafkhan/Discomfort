0ce4889769673a56ce6c85699cb22eb885a55afd
Rafael Khan <raf.khan.f@gmail.com>
2025-04-17 17:34:43 -0400

    here gab

--- firmware/firmware.cpp
+++ firmware/firmware.cpp
@@ -21,6 +21,8 @@ DaisyPatchSM hw;
 Discomfort distChannelL;
 Discomfort distChannelR;
 
+float a = 0;
+
 // Can move into mux class probably
 dsy_gpio muxSelect0;
 dsy_gpio muxSelect1;
@@ -50,35 +52,10 @@ DiscomfortOutput process(float audioIn, DiscomfortHwInputs *hwInputs, Discomfort
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-  // hardwareInputs->readMuxOnePin(&hw, muxPinIdx);
-
-  for(int i = 0; i < 4; i++) {
-    // muxes[0]->setBits(i);
-    // dsy_gpio_write(&muxSelect0, (bool) ((i >> 0) & 1));
-    // dsy_gpio_write(&muxSelect0, (bool) ((i >> 1) & 1));
-    // dsy_gpio_write(&muxSelect0, (bool) ((i >> 2) & 1));
-    asd[i] = hw.adc.GetFloat(0);
-
-    // DiscomfortHwAnalogInput *input = this->readMapper[i][pin];
-    // if(input != nullptr) {
-    //   // input->read(false);
-    //   input->setValue(hw->adc.GetFloat(0));
-    // }
-    // System::Delay(1);
-    // hw->Print("%d, %f \t", i, hw->adc.GetFloat(0));
-  }
-
-
   for (size_t i = 0; i < size; i++)
   {
-    DiscomfortOutput outputL = process(IN_L[i], hardwareInputs, &distChannelL);
-    DiscomfortOutput outputR = process(IN_R[i], hardwareInputs, &distChannelR);
-
-    OUT_L[i] = outputL.audioOutput;
-    OUT_R[i] = outputR.audioOutput;
-
-    // // hw.WriteCvOut(1, 5.f * outputStructL.followerOutput);
-    // // hw.WriteCvOut(2, 5.f * outputStructL.followerOutput);
+    OUT_L[i] = IN_L[i];
+    OUT_R[i] = IN_R[i];
   }
 }
 
@@ -101,12 +78,6 @@ int main(void)
   adc[0].InitSingle(hw.C7);
   adc[1].InitSingle(hw.C6);
   adc[2].InitSingle(hw.C5);
-  // adc[3].InitSingle(hw.C6);
-  // adc[4].InitSingle(hw.C5);
-  // adc[5].InitSingle(hw.C4);
-  // adc[6].InitSingle(hw.C3);
-  // adc[7].InitSingle(hw.C2);
-  // adc[8].InitSingle(hw.C1);
 
   hw.adc.Init(adc, 3);
   hw.adc.Start();
@@ -140,32 +111,21 @@ int main(void)
 
     // this should probably block and fuck things up???
     // inputs = getInputsFromHw(&hw, muxes);
-    // if (muxPinIdx % 8 == 0)
-    // {
-    //   muxPinIdx = 0;
-    // }
-    // muxPinIdx++;
+    if (muxPinIdx % 8 == 0)
+    {
+      muxPinIdx = 0;
+    }
+    muxPinIdx++;
 
-    // hardwareInputs->distEnvAttenuverter->setValue(hw.GetAdcValue(CV_1));
-    // hardwareInputs->foldEnvAttenuverter->setValue(hw.GetAdcValue(CV_2));
-    // System::Delay(1);
-    // printThing(hardwareInputs->foldAmountPot);
+    // Set select pins
+    dsy_gpio_write(&muxSelect0, (bool) ((muxPinIdx >> 0) & 1));
+    dsy_gpio_write(&muxSelect1, (bool) ((muxPinIdx >> 1) & 1));
+    dsy_gpio_write(&muxSelect2, (bool) ((muxPinIdx >> 2) & 1));
 
-    // hw.PrintLine();
-
-    // hardwareInputs->updateAll();
-    // hw.PrintLine(
-    //     "(%d, %d): %f",
-    //     hardwareInputs->foldAmountPot->muxIdx,
-    //     hardwareInputs->foldAmountPot->muxPin,
-    //     fclamp(getScaledPotInput(hardwareInputs->foldAmountPot->getValue()) + getScaledCvInput(hardwareInputs->foldAmountCv->getValue()), 0, 1));
-    // printThing(hardwareInputs->distParamCPot);
-    // printThing(hardwareInputs->distParamCCv);
-    // printThing(hardwareInputs->distParamBPot);
-    // printThing(hardwareInputs->distParamBCv);
-    // printThing(hardwareInputs->foldSymmetryPot);
-    // printThing(hardwareInputs->foldEnvAttenuverter);
-    // printThing(hardwareInputs->foldEnvAttenuverter);
-    // hw.PrintLine("");
+    if(muxPinIdx == 0) {
+      a = hw.adc.GetFloat(0);
+    }
+    float b = hw.adc.GetFloat(1);
+    float c = hw.adc.GetFloat(2);
   }
 }
